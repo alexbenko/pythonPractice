5f1f1e25cd16e79350b9bc2f78b25d5e43816c40
Alex Benko <benkoalex521@gmail.com>
2020-07-21 09:19:36 -0700

    Played around with formatting, string literals, and rounding

--- beginner/formatting.py
+++ beginner/formatting.py
@@ -1,3 +1,4 @@
+#PRacticing wiht string formating, rounding , and string literals
 def poemFormat():
   return "The {1} {0} {2}".format("less","road","traveled")
 
@@ -11,4 +12,15 @@ def welcomeToSite(user):
   user += 1
   return 'You have visited {v} time(s)'.format(v=user)
 
-print(welcomeToSite(0))
+#print(welcomeToSite(0))
+
+#rounds to the third decimal point
+def rounded(amt = .89123):
+  return 'Your final score is {s:1.3f}'.format(s=amt)
+
+print(rounded())
+
+def stringLiteral(string="Alex"):
+  return f"Welcome {string}"
+
+print(stringLiteral())
