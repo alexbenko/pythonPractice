a049af2b34ab98e688f1e9bc2967fe73fd7bdba6
Alex Benko <benkoalex521@gmail.com>
2020-07-21 09:53:42 -0700

    Doubles any number in the given list

---
+++ beginner/lists.py
@@ -0,0 +1,20 @@
+def listPrac(foo):
+  for i in foo:
+    print(i)
+
+#listPrac(["Hello", 123,1.2345])
+
+#if the item in the list is a type of number, double it
+def doubleList(data=["foobar",1,24,"banana"]):
+  for i in range(len(data)):
+    print(i)
+    if isinstance(data[i],int) or isinstance(data[i],float):
+      #first time debugging in python!!
+      print("number")
+      data[i] = data[i] * 2
+    else:
+      continue
+  return data
+
+
+print(doubleList())
