353d4aa6dd9446c722c632893e71dc18c8abd910
Alex Benko <benkoalex521@gmail.com>
2020-07-21 09:09:11 -0700

    Another format practice

--- beginner/formatting.py
+++ beginner/formatting.py
@@ -1,6 +1,14 @@
 def poemFormat():
   return "The {1} {0} {2}".format("less","road","traveled")
 
-print(poemFormat())
+#print(poemFormat())
 #=> The road less traveled
 #similar to string literals in javascript
+
+def welcomeToSite(user):
+  #mock site greeting
+  #in real app this would pull from database
+  user += 1
+  return 'You have visited {v} time(s)'.format(v=user)
+
+print(welcomeToSite(0))
