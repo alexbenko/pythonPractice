8d076d2f83e267e6f2bdfd99507c331d78f0f439
Alex Benko <benkoalex521@gmail.com>
2020-07-25 14:39:42 -0700

    Enumerate keyword

---
+++ misc/enumerate.py
@@ -0,0 +1,12 @@
+def enumerater(word="foobar"):
+  for letter in enumerate(word):
+    print(letter)
+
+enumerater()
+#=>
+#(0, 'f')
+#(1, 'o')
+#(2, 'o')
+#(3, 'b')
+#(4, 'a')
+#(5, 'r')
---
+++ misc/generator.py
@@ -0,0 +1,5 @@
+def generate():
+  return list(range(0,30,3))
+
+print(generate())
+#prints every 3rd number between 0 and 30
