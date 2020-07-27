d9edd2f71a99581a78aaa31dbcf65d40d74eadfc
Alex Benko <benkoalex521@gmail.com>
2020-07-27 02:27:10 -0700

    Returns list of even numbers

--- misc/comprehension.py
+++ misc/comprehension.py
@@ -1,4 +1,10 @@
 def makeListOfLetters(word="foobar"):
   return [letter for letter in word]
 
-print(makeListOfLetters())
+#print(makeListOfLetters())
+
+def evenGenerator(low=0,high=25):
+  return [i for i in range(low,high) if i%2==0]
+
+print(evenGenerator())
+
