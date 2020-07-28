4afcd715c52f4dba4bed81e60ff01df3d34c1e6d
Alex Benko <benkoalex521@gmail.com>
2020-07-27 18:41:03 -0700

    Alternates case of given string

---
+++ toy problems/skyline.py
@@ -0,0 +1,22 @@
+#return a string whever every even letter is uppercase Foobar => fOoBaR
+def skyline(word):
+  output = ""
+  for i in range(len(word)):
+    even = i % 2 == 0
+    print(not even)
+
+    if not even:
+      output += word[i].upper()
+
+    else:
+      output += word[i].lower()
+
+  return output
+
+
+
+
+
+
+print(skyline("anthropomorphism"))
+#[letter for letter in lowered if not ].join()
