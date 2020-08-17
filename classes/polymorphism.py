536d696c0c03c1584e7b4e2aecce7707aad5ba89
Alex Benko <benkoalex521@gmail.com>
2020-08-17 18:01:31 -0400

    Example of polymorphism

---
+++ classes/polymorphism.py
@@ -0,0 +1,27 @@
+#Just a basic example of polymorphism
+class Kitty:
+  def __init__(this,name):
+    this.name = name
+
+  def speak(this):
+    print(f'My name is {this.name} and I am a kitty')
+
+class Doggy:
+  def __init__(this,name,spots):
+    this.name = name
+    this.spots = spots
+
+  def speak(this):
+    print(f'My name is {this.name},I have {this.spots} many spots, and I am a doggy')
+
+
+Goose = Kitty('Goose')
+Kaia = Doggy('Kaia',12)
+
+def poly(thing):
+  thing.speak()
+
+poly(Goose)
+#=>My name is Goose and I am a kitty
+poly(Kaia)
+#=>My name is Kaia,I have 12 many spots, and I am a doggy
