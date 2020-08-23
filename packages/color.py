aad9654a44b3297e03aecd3946235d7bf0053873
Alex Benko <benkoalex521@gmail.com>
2020-08-23 18:11:39 -0400

    Using colorama

--- packages/color.py
+++ packages/color.py
@@ -3,3 +3,6 @@ init()
 from colorama import Fore
 
 print(Fore.RED + 'Some Red Text')
+print(Fore.GREEN + 'Some Green Text')
+print(Fore.BLUE + 'Some Blue Text')
+print(Fore.YELLOW + 'Some Yellow Text')
